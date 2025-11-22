ea2981c048064b0603bd67999c7d2a56d57881ff
manpreetkaur292006-design <manpreetkaur292006@gmail.com>
2025-11-22 17:30:27 +0530

    Author : Manpreet Kaur Adding Comment

---
+++ calc.py
@@ -0,0 +1,7 @@
+# Author : Manpreet Kaur
+# Basic calculator program
+
+'''
+This is a basic calculator program which takes two numbers as an input from the user
+along with the operator of the operation that user want to perform and returns the output.
+'''
