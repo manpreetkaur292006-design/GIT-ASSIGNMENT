7144a50dfcd34b93aa0746060bb7e0df2ca48ae3
manpreetkaur292006-design <manpreetkaur292006@gmail.com>
2025-11-22 17:34:14 +0530

    Author : Manpreet Kaur Adding the welcome messages and user instructions.

--- calc.py
+++ calc.py
@@ -5,3 +5,12 @@
 This is a basic calculator program which takes two numbers as an input from the user
 along with the operator of the operation that user want to perform and returns the output.
 '''
+
+print("Welcome! to the calculator")
+print("operations the this calculator can perform and their corresponding operators:")
+print(" + : Addition")
+print(" - : Subtraction")
+print(" * : Multiplication")
+print(" / : Division")
+print(" // : Floor Division")
+print(" ** : Exponention")
