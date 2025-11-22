0a5a60895870988fccd7c302862978e89c53a022
manpreetkaur292006-design <manpreetkaur292006@gmail.com>
2025-11-22 17:44:01 +0530

    Author : Manpreet Kaur Adding the comditional statements and the calculations

--- calc.py
+++ calc.py
@@ -18,3 +18,27 @@ print(" ** : Exponention")
 num1=float(input("Enter first number:"))
 num2=float(input("Enter second number:"))
 operator=input("Enter the operator:")
+
+if operator=="+":
+    add=num1+num2
+    print("Addition of",num1,"and",num2,":",add)
+
+elif operator=="-":
+    sub=num1-num2
+    print("Subtration of",num1,"and",num2,":",sub)
+
+elif operator=="*":
+    mul=num1*num2
+    print("Multiplication of",num1,"and",num2,":",mul)
+
+elif operator=="/":
+    divi=num1/num2
+    print("Division of",num1,"and",num2,":",divi)
+
+elif operator=="**":
+    exp=num1**num2
+    print(num1,"to the power",num2,":",exp)
+
+elif operator=="//":
+    floor_div=num1//num2
+    print("Floor division of",num1,"and",num2,":",floor_div)
